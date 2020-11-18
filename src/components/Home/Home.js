f4b7730a9c16fa3ee7ceb49c9cc19462cff2ffeb
jinglad <jihanchowdhury70@gmail.com>
2020-11-19 00:04:51 +0600

    basic installation and components created

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header/Header';
+import WhatWeDo from '../WhatWeDo/WhatWeDo';
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <WhatWeDo></WhatWeDo>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/WhatWeDo/WhatWeDo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WhatWeDo = () => {
+    return (
+        <div>
+            <h1>this is what we do</h1>
+        </div>
+    );
+};
+
+export default WhatWeDo;
