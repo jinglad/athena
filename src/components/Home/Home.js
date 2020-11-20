ba22bbccf3763b99886851f8f29fcf2530474916
Rafsun4 <saifrafsun@gmail.com>
2020-11-20 20:53:01 +0600

    done pricing section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import Order from '../Order/Order';
 import WhatWeDo from '../WhatWeDo/WhatWeDo';
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
         <div>
             <Header></Header>
             <WhatWeDo></WhatWeDo>
+            <Order></Order>
             <Footer></Footer>
         </div>
     );
